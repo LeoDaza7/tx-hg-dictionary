43cda04b71989416d549773df23c48f126b26411
Leonardo Daza <leo.daza.769@gmail.com>
2020-09-29 07:00:16 -0400

    Added http get request method to words controllers.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -74,7 +74,16 @@ class WordsController < ApplicationController
     end
 
     def http_request(word)
-      return word
+      headers = { 
+        "app_id" => ENV["APP_ID"],
+        "app_key" => ENV["APP_KEY"]
+      }    
+      response = HTTParty.get(
+        "https://od-api.oxforddictionaries.com/api/v2/words/en-gb?q=" + word,
+        :headers => headers
+      )
+      hash = JSON.parse(response.body)
+      return hash['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
     end
     
 end
